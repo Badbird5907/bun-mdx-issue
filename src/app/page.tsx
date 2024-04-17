86e902d3a645d05b69700c182269abae830e5ce5
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-04-16 21:28:33 -0400

    Remove some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1 className="text-4xl font-bold">Hello World</h1>
       <MDXRemote source={"# Hello, world!"} />
     </main>
